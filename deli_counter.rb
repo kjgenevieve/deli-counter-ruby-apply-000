8b9d58691d6506e24051a2a0586754fc0b5e65f3
Genevieve <kj.genevieve@gmail.com>
2019-07-02 17:19:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,10 +14,22 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+# def take_a_number(katz_deli, name)
+#   katz_deli.push(name)
+#   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+# end
+
+$ticket = 0
+
+def take_a_number(katz_deli)
+  $ticket =+ 1
+  katz_deli.push(ticket)
+  puts "Welcome. You are ticket number #{ticket}."
 end
+# "Welcome, you have ticket #1"
+# "Welcome, you have ticket #2"
+# now_serving must say "Currently serving Ticket # (x)"
+# Welcome, you have ticket #3"
 
 def now_serving(katz_deli)
   if katz_deli.length == 0
@@ -25,6 +37,6 @@ def now_serving(katz_deli)
   else
     next_person = katz_deli[0]
     katz_deli.shift
-    puts "Currently serving #{next_person}."
+    puts "Currently serving ticket number #{next_person}."
   end
 end
